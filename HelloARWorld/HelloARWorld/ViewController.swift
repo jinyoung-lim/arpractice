22d6eaf86e82dde2c6a9ec08941046d155fe9fd8
jlim2 <jlim2@macalester.edu>
2018-10-28 17:33:17 -0500

    Surface detection and drawing ships when tapped

--- HelloARWorld/HelloARWorld/ViewController.swift
+++ HelloARWorld/HelloARWorld/ViewController.swift
@@ -20,15 +20,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
+        // Set the debuging options
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
         // Create a new scene
-        // TODO: change ship.scn to other art assets
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+
         // Set the scene to the view
-        sceneView.scene = scene
+//        sceneView.scene = scene
+        
+        // Enable lighting
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.automaticallyUpdatesLighting = true
+        
+        // Recognize tap gesture
+        addTapGestureToSceneView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -36,10 +45,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
+        // Horizontal plane detection
+        configuration.planeDetection = .horizontal
         // Run the view's session
         sceneView.session.run(configuration)
     }
+    
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
@@ -50,14 +62,77 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    //Override to create and configure nodes for anchors added to the view's session.
+    // Visualize horizontal planes refer: https://www.appcoda.com/arkit-horizontal-plane/
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        // Unwrap the anchor as an ARPlaneAnchor
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        // Get the anchor dimension
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        let plane = SCNPlane(width: width, height: height)
+        
+        // Set the color of the plane
+        plane.materials.first?.diffuse.contents = UIColor.magenta
+        
+        // Initialize a SCNNode with the geometry we got
+        let planeNode = SCNNode(geometry: plane)
+        
+        // Initialize coordinates of the plane
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+        planeNode.position = SCNVector3(x,y,z)
+        planeNode.eulerAngles.x = -.pi / 2
+        
+        // Add the plane node as a SceneKit
+        node.addChildNode(planeNode)
     }
-*/
+    
+    // Renderer to expand the horizontal planes
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        // First, unwrap the plane anchor as ARPlaneAnchor, then
+        // unwrap the first plane node, then
+        // unwrap the node geometry
+        guard let planeAnchor = anchor as? ARPlaneAnchor,
+            let planeNode = node.childNodes.first,
+            let plane = planeNode.geometry as? SCNPlane
+            else { return }
+        
+        // Update plane geometry
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        plane.width = width
+        plane.height = height
+        
+        // Update plane coordinates
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+        planeNode.position = SCNVector3(x, y, z)
+    }
+    
+    
+    
+    @objc func addShipToSceneView(withGestureRecognizer recognizer: UIGestureRecognizer) {
+        let tapLocation = recognizer.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
+        
+        guard let hitTestResult = hitTestResults.first else { return }
+//        let translation = hitTestResult.worldTransform.translation
+        let translation = hitTestResult.worldTransform.columns.3
+        let x = translation.x
+        let y = translation.y
+        let z = translation.z
+        
+        guard let shipScene = SCNScene(named: "art.scnassets/ship.scn"),
+            let shipNode = shipScene.rootNode.childNode(withName: "ship", recursively: false)
+            else { return }
+        
+        shipNode.position = SCNVector3(x,y,z)
+        sceneView.scene.rootNode.addChildNode(shipNode)
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
@@ -72,5 +147,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+    
+    // Detect tap gesture
+    func addTapGestureToSceneView() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.addShipToSceneView(withGestureRecognizer:)))
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
     }
 }
--- HelloARWorld/README.md
+++ HelloARWorld/README.md
@@ -1,5 +1,13 @@
-#HelloARWorld
+# HelloARWorld
 
-First Augmented Reality App build by following https://mobile-ar.reality.news/how-to/arkit-101-get-started-building-augmented-reality-application-iphone-ipad-quickly-0177989/. Simple 3D model viewing without surface detection or other interactions.
+First Augmented Reality app build by following https://mobile-ar.reality.news/how-to/arkit-101-get-started-building-augmented-reality-application-iphone-ipad-quickly-0177989/, which walks through creating an AR Project on XCode.
 
-/Users/JJ/cs465/arpractice/HelloARWorld/hello_ar_world.PNG
+##### 1. Simple 3D model viewing without surface detection or other interactions
+
+![AR Ship Model View](../HelloARWorld/hello_ar_world.PNG)
+
+
+##### 2. Surface Detection and drawing ships when tapped
+https://blog.markdaws.net/arkit-by-example-part1-7830677ef84d
+![AR Ship Model View](../HelloARWorld/ship_on_plane.PNG)
+![AR Ship Model View](../HelloARWorld/ship_on_plane_two.jpeg)
